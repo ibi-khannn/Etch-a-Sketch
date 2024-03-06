4603f6b7817893ded05a3a224745665ecaa1ceb7
Ibi <146767915+ibi-khannn@users.noreply.github.com>
2024-03-06 18:15:20 +0500

    Added comments so that the code makes more sense

--- project.js
+++ project.js
@@ -63,15 +63,15 @@ function makeGrid (gridDimensions) {
     parentDiv.classList.add("container");
     document.body.appendChild(parentDiv);
     
-    for (let i = 1; i <= gridDimensions; i++) {
+    for (let i = 1; i <= gridDimensions; i++) { // what we are doing is we are creating rows and inside those rows we are creating the boxes, this can be seen by removing the border and background color properties from row class and adding them inside the col class. The structure will be made clearer.
         const col = document.createElement("div");
         col.classList.add("cols");
             for (let j = 1; j <= gridDimensions; j++) {
-                const row = document.createElement("div");
+                const row = document.createElement("div"); // each new div (which will be the box inside the grid, i.e. 5 x 5 grid will have 25 boxes) is appended inside the row created above until it ends, then the inner loop ends and new row is created and the process continues
                 row.classList.add("rows");
                 col.appendChild(row);
             }
-        parentDiv.appendChild(col);
+        parentDiv.appendChild(col); // as the parentDiv is given flex-direction: column, the columns are created under each other which are essentially rows to be exactly
         }
         
         parentDivRef = parentDiv; // STORING CONTAINER (PARENTDIV) TO BE DELETED WHEN FUNCTION CALLED AGAIN
@@ -81,10 +81,10 @@ function makeGrid (gridDimensions) {
         
             allChildDivs.forEach((eachDiv) => { // have to keep this forEach loop inside the functions so that color changing works on new grid
                 eachDiv.addEventListener("mouseenter", () => {
-                    eachDiv.style.backgroundColor = "black";
+                    eachDiv.style.backgroundColor = "blue";
                 })
                 eachDiv.addEventListener("mouseleave", () => {
-                    eachDiv.style.backgroundColor = "black";
+                    eachDiv.style.backgroundColor = "white";
                 })
                 eachDiv.addEventListener ("click", () => { // use the if else statement inside the click event function to have the color selected inside the div after a click
                     let newColor = getRandomColor();
